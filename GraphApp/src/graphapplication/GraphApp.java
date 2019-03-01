307ed2398b9263df6c7b8ffa6342e8e14252b546
"Danilson Sanches" <"dsanches06@outlook.com">
2019-03-01 10:06:37 +0000

    update directory

--- GraphApp/src/graphapplication/GraphApp.java
+++ GraphApp/src/graphapplication/GraphApp.java
@@ -33,11 +33,11 @@ public class GraphApp extends Application {
         painel.iniciar();
 
         Scene scene = new Scene(root, 800, 600);
-        scene.getStylesheets().addAll(this.getClass().getResource("/pt/ips/pa/projeto/varianteb/styles/style.css").toExternalForm());
+        scene.getStylesheets().addAll(this.getClass().getResource("/graphapplication/styles/style.css").toExternalForm());
 
         Stage stage = new Stage(StageStyle.DECORATED);
         stage.setTitle("Aplicação Didática sobre Grafos - Época Recurso – Variante B");
-        stage.getIcons().add(new Image("/pt/ips/pa/projeto/varianteb/styles/favicon.png"));
+        stage.getIcons().add(new Image("/graphapplication/styles/favicon.png"));
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
